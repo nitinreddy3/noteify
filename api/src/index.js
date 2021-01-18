156d34fbb387133cdf52115cfb716b6d4a45ac5c
nitin reddy <reddynitin3@gmail.com>
2021-01-18 19:54:03 +0530

    Wrote a first mutation for GraphQL:

--- api/src/index.js
+++ api/src/index.js
@@ -6,19 +6,50 @@ const app = express()
 const { ApolloServer, gql } = require('apollo-server-express')
 const port = process.env.PORT || 4000
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 // Construct the schema object
+
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
   type Query {
     hello: String,
-    firstName: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 // Construct a resolver function
 const resolvers = {
+
   Query: {
     hello: () => 'Hello world!',
-    firstName: () => 'Nitin Reddy'
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id)
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      }
+      notes.push(noteValue)
+      return noteValue
+    }
   }
 }
 
